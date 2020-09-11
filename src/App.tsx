b2dcf7b1020335d71010a91ab7d15f0ed51cba7d
YvonVuijk <ymvuijk@hotmail.com>
2020-09-11 13:01:25 +0200

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from "react-query";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Country } from "./types/Country";
+import style from "./App.module.scss";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from 'react-router-dom';
+
+import ThemeToggle from './components/ThemeToggle';
+import Home from './pages/Home';
+import SinglePage from './pages/SinglePage';
+
+export default function App() {
+  const [countryMap, setCountryMap] = useState<{ [key: string]: string }>();
+
+  const { data, isLoading, error } = useQuery<Country[]>(
+    "countries",
+    fetchCountries
   );
-}
 
-export default App;
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const countryMap: { [key: string]: string } = {};
+      for (const country of data) {
+        if (!countryMap[country.alpha3Code]) {
+          countryMap[country.alpha3Code] = country.name!;
+        }
+      }
+
+      setCountryMap(countryMap);
+    }
+  }, [data, isLoading, error]);
+
+  return (
+    <Router>
+      <div className={style.container}>
+
+        <header className={style.header}>
+          <div className={style.content}>
+            <Link to='/'>Where in the world?</Link>
+
+            <div>
+              <ThemeToggle />
+            </div>
+
+          </div>
+        </header>
+
+        <div className={style.container}>
+          <Switch>
+            <Route path="/" exact >
+              <Home countries={data} />
+            </Route>
+            <Route path="/countries/:id" >
+              <SinglePage countryMap={countryMap} />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+
+        </div>
+
+      </div>
+    </Router>
+  );
+};
+
+const fetchCountries = () =>
+  fetch("https://restcountries.eu/rest/v2/all").then((res) => res.json())
---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Country } from "../types/Country";
+
+import { formatNumber } from '../helpers/utilities';
+
+import Filter from '../components/Filter';
+
+import Grid from '@material-ui/core/Grid';
+
+type HomeProps = {
+  countries?: Country[];
+};
+
+const Home: React.FC<HomeProps> = ({ countries }) => {
+  return (
+    <>
+
+      {/* Filter options */}
+      <Grid container className='filter-row'>
+        <Grid item xs={9}>
+          <input type='text' placeholder='Search for a country...' className='input-search' />
+        </Grid>
+        <Grid item xs={3}>
+          <Filter />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={3}>
+        {countries && (
+          countries.map((country, i) => (
+            <Grid item xs={3} key={i}>
+
+              <Link to={`countries/${country.alpha3Code}`} >
+                <img src={country.flag} alt={`Flag of ${country.name}`} />
+              </Link>
+
+              <div>
+                <h5>{country.name}</h5>
+                <p><strong>Population:</strong> {formatNumber(country.population)}</p>
+                <p><strong>Region:</strong> {country.region}</p>
+                <p><strong>Capital:</strong> {country.capital}</p>
+              </div>
+
+            </Grid>
+          ))
+        )}
+
+      </Grid>
+
+    </>
+
+  );
+};
+
+export default Home;
